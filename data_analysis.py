16b3c4da5afd73f8b4be4896073d3b331e6ebe25
Kevin Wang <kwang2@stanford.edu>
2017-05-02 20:33:50 -0700

    finished my lab.

--- data_analysis.py
+++ data_analysis.py
@@ -14,8 +14,24 @@ def main():
     """This function sould call noisy_packet() to get a Gaussian wave
     packet, call clean_data() to apply a low pass filter to the data and
     finally plot the result."""
-    #TODO add your code here
-
+    x_values = np.arange(-np.pi, np.pi, 0.01)
+    noisy_y = noisy_packet(x_values, k = 5, sigma = 1, noise_amplitude = 0.2)
+    clean_y = wavepacket(x_values,k = 5, sigma = 1)
+    noisy_cleaned_y = clean_data(x_values,noisy_y)
+    
+    plt.figure(1)
+    plt.title('original wave')
+    plt.plot(x_values, clean_y)
+    
+    plt.figure(2)
+    plt.title('noisy wave')
+    plt.plot(x_values, noisy_y)
+    
+    plt.figure(3)
+    plt.title('cleaned wave')
+    plt.plot(x_values, noisy_cleaned_y)
+    
+    plt.show()
 
 def noisy_packet(x_values, k, sigma, noise_amplitude):
     """This function returns a noisy Gaussian wavepacket with wave
@@ -29,8 +45,15 @@ def clean_data(x_values,y_values):
     """This function should take a set of y_values, perform the Fourier
     transform on it, filter out the high frequency noise, transform the
     signal back into real space, and return it."""
-
-    pass #TODO add your code here
-    #return y_clean
+    y_fft = np.fft.rfft(y_values) # Fourier transform
+    
+    low_pass_filter = np.ones(y_fft.shape) # build low pass filter for Fourier function
+    num_freq = y_fft.size
+    low_pass_filter[num_freq//25:num_freq] = 0
+    
+    y_clean_fft = np.multiply(y_fft, low_pass_filter) # perform low pass filter
+    
+    y_clean_values = np.fft.irfft(y_clean_fft, len(y_values)) # Inverse Fourier transform
+    return y_clean_values
 
 main()  # calls your main function
--- trig.py
+++ trig.py
@@ -5,9 +5,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# TODO fill in this function
 def integrate(y, dx):
-    pass
+    return np.dot(y, dx)
 
 # TODO write code here to setup arrays x and y = sin(x) and then plot them.
 # After this is done implement your integrate function above integrate y
+x = np.arange(0, np.pi, 0.01)
+
+y_sin = np.sin(x)
+plt.figure(1)
+plt.title('y=sin(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(x, y_sin)
+
+
+y_cos = np.cos(x)
+plt.figure(2)
+plt.title('y=cos(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(x, y_cos)
+
+dx = np.full(x.shape, 0.01)
+print("result of integrate(y, dx):")
+print("sin:", integrate(y_sin, dx))
+print("cos:", integrate(y_cos, dx))
+
+print("result of numpy.trapz:")
+print("sin:", np.trapz(y_sin, dx=0.01))
+print("cos:", np.trapz(y_cos, dx=0.01))
+
+plt.show()
